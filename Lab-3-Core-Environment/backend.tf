d09ad82e0f8f616764aab9f4a72e09410c53a71a
Liam F. O'Neill <liamfoneill@outlook.com>
2020-03-20 08:43:45 +0000

    updated backends to prod storage account

--- Lab-3-Core-Environment/backend.tf
+++ Lab-3-Core-Environment/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "Terraform-State-Storage-North-Europe"
-    storage_account_name  = "tfstatestorenortheurope"
+    resource_group_name   = "Core-Infrastructure-Production"
+    storage_account_name  = "liamcoreinfraprod"
     container_name        = "terraformstate"
     key                   = "lab-3-core-environment.tfstate"
   }
